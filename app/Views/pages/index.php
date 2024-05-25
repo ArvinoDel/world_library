160967ad6714037d490d55f8eaacf5b33c8b646e
Faiz Firmansyah <145418503+m-faiz07@users.noreply.github.com>
2024-05-25 20:33:04 +0700

    perubahan faiz

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -5,7 +5,7 @@
         <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="w-100" src="<?= base_url('img/bg-library0.jpg'); ?>" alt="Image">
+                    <img class="w-100" src="<?= base_url('img/bg-library.jpg'); ?>" alt="Image">
                     <div class="carousel-caption">
                         <div class="container">
                             <div class="row justify-content-start">
@@ -308,6 +308,87 @@
     </div>
     <!-- Gallery End -->
 
+
+    <!-- Product Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                <p class="section-title bg-white text-center text-primary px-3">Our Books</p>
+                <h1 class="mb-5">Wanna Buy Some Books?</h1>
+            </div>
+            <div class="row gx-4">
+                <div class="col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="product-item">
+                        <div class="position-relative" style="justify-content: center; text-align: center;">
+                            <img class="img-fluid" src="<?= base_url('img/buku12.jpeg'); ?>" alt="">
+                            <div class="product-overlay">
+                                <a class="btn btn-square btn-secondary rounded-circle m-1" href=""><i class="bi bi-link"></i></a>
+                                <a class="btn btn-square btn-secondary rounded-circle m-1" href=""><i class="bi bi-cart"></i></a>
+                            </div>
+                        </div>
+                        <div class="text-center p-4">
+                            <a class="d-block h5" href="">Hide And Seek</a>
+                            <span class="text-primary me-1">$19.00</span>
+                            <span class="text-decoration-line-through">$29.00</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="product-item">
+                        <div class="position-relative" style="justify-content: center; text-align: center;">
+                            <img class="img-fluid" src="<?= base_url('img/buku13.jpeg'); ?>" alt="">
+                            <div class="product-overlay">
+                                <a class="btn btn-square btn-secondary rounded-circle m-1" href=""><i class="bi bi-link"></i></a>
+                                <a class="btn btn-square btn-secondary rounded-circle m-1" href=""><i class="bi bi-cart"></i></a>
+                            </div>
+                        </div>
+                        <div class="text-center p-4">
+                            <a class="d-block h5" href="">Alone</a>
+                            <span class="text-primary me-1">$19.00</span>
+                            <span class="text-decoration-line-through">$29.00</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="product-item">
+                        <div class="position-relative" style="justify-content: center; text-align: center;">
+                            <img class="img-fluid" src="<?= base_url('img/buku14.jpeg'); ?>" alt="">
+                            <div class="product-overlay">
+                                <a class="btn btn-square btn-secondary rounded-circle m-1" href=""><i class="bi bi-link"></i></a>
+                                <a class="btn btn-square btn-secondary rounded-circle m-1" href=""><i class="bi bi-cart"></i></a>
+                            </div>
+                        </div>
+                        <div class="text-center p-4">
+                            <a class="d-block h5" href="">Strategic Inovation</a>
+                            <span class="text-primary me-1">$19.00</span>
+                            <span class="text-decoration-line-through">$29.00</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="product-item" style="height: 95%;">
+                        <div class="position-relative" style="justify-content: center; text-align: center;">
+                            <img class="img-fluid" src="<?= base_url('img/buku15.jpeg'); ?>" alt="">
+                            <div class="product-overlay">
+                                <a class="btn btn-square btn-secondary rounded-circle m-1" href=""><i class="bi bi-link"></i></a>
+                                <a class="btn btn-square btn-secondary rounded-circle m-1" href=""><i class="bi bi-cart"></i></a>
+                            </div>
+                        </div>
+                        <div class="text-center p-4">
+                            <a class="d-block h5" href="">The Girl in The Woods</a>
+                            <span class="text-primary me-1">$19.00</span>
+                            <span class="text-decoration-line-through">$29.00</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Product End -->
+
+
+
+
     <!-- Testimonial Start -->
     <div class="container-xxl py-5">
         <div class="container">
@@ -315,7 +396,6 @@
                 <p class="section-title bg-white text-center text-primary px-3">Testimonial</p>
                 <h1 class="mb-5">What People Say About Our WorldLibrary</h1>
             </div>
-           <!-- TOLONG INI NANTI DIISI, JANGAN ISI MAKE DULL DATA YAA -->
             <div class="row g-5 align-items-center">
                 <div class="col-lg-5 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="testimonial-img">
