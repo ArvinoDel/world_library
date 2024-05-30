0dab81812ad09edc24db5291a48f41f31634c426
Andika <andikasupriyadi27@gmail.com>
2024-05-30 14:54:24 +0700

    update

--- app/Views/layout/template2.php
+++ app/Views/layout/template2.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= base_url('css/style5.css'); ?>">
+    <link rel="shortcut icon" href="<?= base_url('img/logo-perpus.png') ?>" type="image/x-icon">
+    <link rel="shortcut icon" href="<?= base_url('img/logo-perpus.png') ?>" type="image/x-icon">
 
     <!----===== Iconscout CSS ===== -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
